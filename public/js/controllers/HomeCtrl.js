e7ed94d2bb308d26dbb6d56b80127694d792e582
Leonardgrr <leonardgrr@fullsail.edu>
2015-10-26 21:17:51 -0400

    initial setup of project, trying to integreate angular material framework

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,9 +1,29 @@
 angular.module('foto')
-.controller('HomeCtrl', ["$scope", "$http", function($scope, $http){
+.controller('HomeCtrl', ["$scope", "$http", "$firebaseArray", function($scope, $http, $firebaseArray){
 	console.log("Hola Amigo")
 
-	$http.get('js/data.json').success(function(data){
-		$scope.artists = data
-		console.log($scope.artists);
-	})
+	var ref = new Firebase("https://smsfoto.firebaseio.com");
+	$scope.artists = $firebaseArray(ref);
+
+	$scope.postComment = function(){
+		$scope.artists.$add({
+			author : $scope.newComment.author,
+			body: $scope.newComment.body
+		})
+	}
+
+	$scope.removeComment = function(obj){
+		$scope.artists.$remove(obj).then(function(ref){
+			ref.key() === obj.$id; // true
+		})
+	}
+
+	$scope.imagePath = 'imgs/mountain.jpg';
+
+
+	// get the local json object
+	// $http.get('js/data.json').success(function(data){
+	// 	$scope.artists = data
+	// 	console.log($scope.artists);
+	// })
 }]);
